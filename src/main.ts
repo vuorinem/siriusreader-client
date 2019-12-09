213732d39ac3e0c075c9072a5082e9d9a177da11
Mikko Vuorinen <vuorinem@gmail.com>
2019-12-09 20:23:26 +0000

    Add global dialog configuration

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import * as environment from '../config/environment.json';
 import {PLATFORM} from 'aurelia-pal';
 
 import 'styles/app.scss';
+import { DialogConfiguration } from 'aurelia-dialog';
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .plugin(PLATFORM.moduleName("aurelia-animator-css"))
-    .plugin(PLATFORM.moduleName('aurelia-dialog'));
+    .plugin(PLATFORM.moduleName('aurelia-dialog'), (config: DialogConfiguration) => {
+      config.useDefaults();
+      config.settings.keyboard = true;
+      config.settings.overlayDismiss = true;
+    });
 
   aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
