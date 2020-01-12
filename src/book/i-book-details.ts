69f2b062b70a45712292b6a5b576caf9de9f6020
Mikko Vuorinen <vuorinem@gmail.com>
2020-01-12 00:49:41 +0000

    Add missing book details properties

--- src/book/i-book-details.ts
+++ src/book/i-book-details.ts
@@ -2,5 +2,8 @@ import { ISectionDetails } from './i-section-details';
 
 export interface IBookDetails {
     bookId: number;
+    title: string;
+    author: string;
+    description: string;
     sections: ISectionDetails[];
 }
