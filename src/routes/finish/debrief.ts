351988aefeb0d8b172a169902b534cd0b4681a3c
Mikko Vuorinen <vuorinem@gmail.com>
2020-02-08 22:21:57 +0000

    Show user's motivation condition in debrief page

--- src/routes/finish/debrief.ts
+++ src/routes/finish/debrief.ts
@@ -1,2 +1,15 @@
+import { UserService } from 'user/user-service';
+import { autoinject, computedFrom } from 'aurelia-framework';
+
+@autoinject
 export class Debrief {
+
+  @computedFrom('userService.user')
+  private get isIntrinsic(): boolean {
+    return this.userService.user.isIntrinsicCondition;
+  }
+
+  constructor(private userService: UserService) {
+  }
+
 }
--- src/user/i-user-details.ts
+++ src/user/i-user-details.ts
@@ -7,4 +7,5 @@ export interface IUserDetails {
   isBookFinished: boolean;
   readingSpeedWordsPerMinute: number;
   answeredQuestionnaires: string[];
+  isIntrinsicCondition: boolean | null;
 }
