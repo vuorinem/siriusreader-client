455b07297aa9314d3ead7b31e5d67e8181a8f48b
Mikko Vuorinen <vuorinem@gmail.com>
2021-02-14 16:09:56 +0000

    Add footer

--- src/routes/public/nr-footer.ts
+++ src/routes/public/nr-footer.ts
@@ -3,7 +3,7 @@ import { Router } from 'aurelia-router';
 import { autoinject } from 'aurelia-framework';
 
 @autoinject
-export class NrHeader {
+export class NrFooter {
   constructor(
     private router: Router,
     private authService: AuthService) {
