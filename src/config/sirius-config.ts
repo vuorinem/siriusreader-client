80692fbf8d155b111d9d38587b37287367d1cc8a
Mikko Vuorinen <vuorinem@gmail.com>
2021-02-21 23:11:47 +0000

    Fix reading environment variables into configuration

--- src/config/sirius-config.ts
+++ src/config/sirius-config.ts
@@ -1,11 +1,10 @@
-import { env } from 'process';
 import * as environment from '../../config/environment.json';
 
 export const SiriusConfig = {
-  debug: !!(process.env.debug || environment.debug),
-  testing: !!(process.env.testing || environment.testing),
-  apiUrl: process.env.apiUrl || environment.apiUrl,
-  tokenEndpoint: process.env.tokenEndpoint || environment.tokenEndpoint,
-  isHighlightingEnabled: !!(process.env.isHighlightingEnabled || environment.isHighlightingEnabled),
-  isRegistrationDisabled: !!(process.env.isRegistrationDisabled || environment.isRegistrationDisabled),
+  debug: !!(process.env.debug ?? environment.debug),
+  testing: !!(process.env.testing ?? environment.testing),
+  apiUrl: process.env.apiUrl ?? environment.apiUrl,
+  tokenEndpoint: process.env.tokenEndpoint ?? environment.tokenEndpoint,
+  isHighlightingEnabled: !!(process.env.isHighlightingEnabled ?? environment.isHighlightingEnabled),
+  isRegistrationDisabled: !!(process.env.isRegistrationDisabled ?? environment.isRegistrationDisabled),
 };
