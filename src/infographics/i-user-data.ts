9458a3b858ea3a709ad6d785e0e1eb4fa87f12cd
Mikko Vuorinen <vuorinem@gmail.com>
2021-04-06 18:51:38 +0100

    Periodically update infographic status

---
+++ src/infographics/i-user-data.ts
@@ -0,0 +1,4 @@
+export interface IUserData {
+  totalEngagedReadingMinutes: number;
+  isInfographicReady: boolean;
+}
--- src/infographics/infographic-service.ts
+++ src/infographics/infographic-service.ts
@@ -4,18 +4,18 @@ import { autoinject } from "aurelia-dependency-injection";
 import { HttpClient } from "aurelia-fetch-client";
 import { AuthService } from "auth/auth-service";
 import { EventAggregator } from 'aurelia-event-aggregator';
-
-const InfographicUpdateIntervalInSeconds = 60 * 5;
+import { IUserData } from './i-user-data';
 
 @autoinject
 export class InfographicService {
   public isInfographicReady: boolean = false;
+  public totalEngagedReadingMinutes: number = 0;
 
   constructor(
     private http: HttpClient,
     private eventAggregator: EventAggregator,
     private authService: AuthService) {
-    
+
     this.tryRefresh();
 
     window.setInterval(() => this.tryRefresh(), SiriusConfig.infographicRefreshIntervalInSeconds * 1000);
@@ -31,7 +31,13 @@ export class InfographicService {
   }
 
   private async updateInfographic() {
-    // TODO: Call infographic API
-    this.isInfographicReady = !this.isInfographicReady;
+    const response = await this.http.fetch(`/infographic/update`, {
+      method: 'post',
+    });
+
+    const userData: IUserData = await response.json();
+
+    this.isInfographicReady = userData.isInfographicReady;
+    this.totalEngagedReadingMinutes = userData.totalEngagedReadingMinutes;
   }
 }
