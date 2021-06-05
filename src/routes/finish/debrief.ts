23e8248047aea6e8137937755fea3dd4d4f7283c
Mikko Vuorinen <vuorinem@gmail.com>
2021-06-05 14:49:07 +0100

    Update debrief page

--- src/routes/finish/debrief.ts
+++ src/routes/finish/debrief.ts
@@ -1,19 +1,2 @@
-import { UserService } from 'user/user-service';
-import { autoinject, computedFrom } from 'aurelia-framework';
-
-@autoinject
 export class Debrief {
-
-  @computedFrom('userService.user')
-  private get isIntrinsic(): boolean {
-    if (!this.userService.user) {
-      return false;
-    }
-
-    return this.userService.user.isIntrinsicCondition ?? false;
-  }
-
-  constructor(private userService: UserService) {
-  }
-
 }
